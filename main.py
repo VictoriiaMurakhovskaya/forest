addddcdb07dd31a826f2028d0d4d957e7e1f9e2f
Victoriia Murakhovska <inbox@victoriia.online>
2021-05-16 00:34:18 +0300

    Update with activity and sequence diagram

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
+from threading import Thread
+from concurrent.futures import ThreadPoolExecutor as tpe
+from queue import Queue
 
 
 class NatureEntity(ABC):
@@ -20,6 +23,18 @@ class NatureEntity(ABC):
 
     @abstractmethod
     def die(self):
+        """
+        Абстрактний метод вмирання, що перезавантажується у класах нащадках
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def breath(self):
+        """
+        Абстрактний метод дихання, що перезавантажується у класах нащадках
+        :return:
+        """
         pass
 
     @abstractmethod
@@ -43,9 +58,44 @@ class Animals(NatureEntity):
         self.parent = parent
         self.population = 0
         self.health = self.health_states[0]
-        self.alive = True
-        self.hungry = True
-        self.thirsty = True
+        self.components = []
+        self.descendants = []
+        self._alive = True
+        self._hungry = True
+        self._thirsty = True
+        self._alive = True
+        self._tired = True
+
+    @property
+    def thirsty(self):
+        return self._thirsty
+
+    @thirsty.setter
+    def thirsty(self, value):
+        self._thirsty = value
+
+    @property
+    def hungry(self):
+        return self._hungry
+
+    @hungry.setter
+    def hungry(self, value):
+        self._hungry = value
+
+    @property
+    def alive(self):
+        if self.health in self.health_states[:-1]:
+            return True
+        else:
+            return False
+
+    @property
+    def tired(self):
+        return self._tired
+
+    @tired.setter
+    def tired(self, value):
+        self._tired = value
 
     def move(self):
         """
@@ -62,6 +112,7 @@ class Animals(NatureEntity):
         if self.thirsty:
             return False
         else:
+            self.hungry = False
             return True
 
     def die(self):
@@ -70,6 +121,8 @@ class Animals(NatureEntity):
         :return:
         """
         if self.health == 'ready_to_die':
+            elements = self.decompose()
+            self.parent.elements.put(elements)
             return True
         else:
             return False
@@ -80,35 +133,106 @@ class Animals(NatureEntity):
         :return:
         """
         if not self.alive:
+            return self.components
+        else:
+            return None
+
+    def drink(self):
+        """
+        Метод пиття. Отримує воду від абіотичних факторів
+        :return:
+        """
+        water = self.parent.subsystems['abiotics'].get_water()
+        self.thirsty = False
+        return True
+
+    def eat_herb(self):
+        """
+        Метод поїдання трав. Отримує їх від консументів
+        :return:
+        """
+        herbs = self.parent.subsystems['producers'].give_herb()
+
+    def reproduce(self, pr):
+        """
+        Метод розмноження
+        :param pr: партнер
+        :return:
+        """
+        if self.hungry | self.thirsty:
+            return False
+        if self.health in self.health_states[1:]:
+            return False
+        if (self.fertilize()) & (self.get_descendant(pr)):
+            self.tired = True
             return True
         else:
             return False
 
-    def drink(self):
-        pass
-
-    def eat_herb(self):
-        pass
-
-    def reproduce(self):
-        if self.hungry | self.thirsty:
-            return False
+    def get_descendant(self, pr):
+        """
+        Отримання нащадка від партнера
+        Додає до списку власних нащадків
+        :param pr: партнер
+        :return: нащадка
+        """
+        if desc := pr.give_descendant():
+            self.descendants.append(desc)
         return True
 
-    def live(self):
-        pass
-
-    def sleep(self):
-        pass
-
-    @abstractmethod
-    def multiply(self):
+    def give_descendant(self):
         """
-        Розмножування. Притаманне усім тваринам.
-        Але у різний спосіб. Тому метод повинен бути перезавантажений у конкретних класів тварин
+        Метод, що дає нащадка партнерові
         :return:
         """
-        pass
+        return True
+
+    def sleep(self):
+        """
+        Метод спати. Спрацьовує, якщо нічний час або втома
+        :return:
+        """
+        while self.alive:
+            if not daytime(datetime.now()) | self.tired:
+                pass
+            else:
+                pass
+
+    def breath(self):
+        """
+        Метод дихати
+        :return:
+        """
+        while self.alive:
+            pass
+
+    def fertilize(self):
+        """
+        Метод дати елементи живлення
+        :return:
+        """
+        return True
+
+    def find_partner(self):
+        """
+        Метод пошуку партнера
+        :return:
+        """
+        return Animals()
+
+    def main_life_cycle(self):
+        """
+         Життєвий цикл консументу
+        :return:
+        """
+        while self.alive:
+            while self.thirsty:
+                self.drink()
+            while pr.hungry:
+                self.eat()
+            self.reproduce(self.find_partner())
+        self.die()
+        self.decompose()
 
 
 class Plants(NatureEntity):
@@ -144,7 +268,6 @@ class Plants(NatureEntity):
         Перезавантаження абстрактного методу
         :return:
         """
-        pass
 
     def die(self):
         """
@@ -158,6 +281,9 @@ class Plants(NatureEntity):
         Перезавантаження абстрактного методу
         :return:
         """
+        pass
+
+    def breath(self):
         pass
 
 
@@ -190,15 +316,15 @@ class Predators(Animals):
         super().__init__(name, parent)
         self.parent = parent
 
-    def multiply(self):
-        """
-        Перезавантаження методу розмноження
-        :return:
-        """
-        pass
-
     def hunt(self):
-        pass
+        return True
+
+    def eat(self):
+        if self.hunt():
+            self.hungry = False
+            return True
+        else:
+            return False
 
 
 class Phytophages(Animals):
@@ -289,6 +415,13 @@ class Producers:
 
     def add_tree(self, tree):
         self.trees.append(tree)
+
+    def give_herb(self):
+        pass
+
+    def get_nutrients(self):
+        if not self.parent.elements.empty():
+            pass
 
 
 class Consumers:
@@ -414,6 +547,9 @@ class Forest:
         """
         self.subsystems = systems
 
+        # елементи системи
+        self.elements = Queue()
+
         # властивості системи
         self._weight = 0
         self._balance = True
@@ -443,25 +579,39 @@ class Forest:
         return self._balance
 
     def sunrise(self):
+        """
+        Схід сонця
+        :return:
+        """
         self.sun = True
         self.temp = 19
 
     def sunset(self):
+        """
+        Захід сонця
+        :return:
+        """
         self.sun = False
         self.temp = 17
 
     def crush(self):
+        """
+        Crush системи
+        :return:
+        """
         pass
 
     def check_requirements(self):
+        """
+        Перевірка вимог до підсистем
+        :return:
+        """
         # вимоги до підсистеми Consumers
         producers_weight = self.subsystems['producers'].weight()
         phytophages_weight = self.subsystems['consumers'].phytophages.weight()
         predators_weight = self.subsystems['consumers'].predators.weight()
         assert phytophages_weight == 0.1 * producers_weight
         assert predators_weight == 0.1 * phytophages_weight
-
-        # вимоги до підсистеми Detriophages
 
         # вимоги до підсистеми AbioticFactors
         assert self.subsystems['abiotics'].daytemp() > 18
@@ -470,6 +620,10 @@ class Forest:
         assert self.subsystems['abiotics'].nighttemp() < 16
 
     def light_photosyntesis_phase(self):
+        """
+        Перевіряє, чи фаза світлового фотосинтезу є поточною
+        :return:
+        """
         self.time = datetime.now()
         if (self.time.hour > 6) & (self.time.hour < 19):
             return True
@@ -478,16 +632,56 @@ class Forest:
 
 
 def weight_calculate(lst):
+    """
+    Визначення сумарної маси
+    :param lst: елементи системи
+    :return: числове значення сумарної маси
+    """
     return sum([item.weight for item in lst])
 
 
+def daytime(time):
+    """
+    Повертає True, якщо переданий параметр є денним часом
+    :param time: час, який треба визначити
+    :return:
+    """
+    return True
+
+
 if __name__ == '__main__':
+    # створення екземплярів об'єктів
     f = Forest()
     herb1 = Herbs('Herb1', f)
     herb2 = Herbs('Herb2', f)
-    producers = Producers(f)
-    consumers = Consumers(f)
+    pp = Phytophages('Миша', parent=None)
+    pr = Predators('Вовк', parent=None)
+    producer = Producers(f)
+    consumer = Consumers(f)
+    consumer.phytophages = [pp]
+    consumer.predators = [pr]
     detr1 = Insects('Insect', f)
     detr2 = Worms('Worms', f)
     detrio = Detriophages(f, [detr1, detr2])
     abio = AbioticFactors(f)
+
+    # відтворення життєвих циклів
+    # на основі Sequence diagram
+    # запуск в паралельному потоці життєвого циклу консументу
+    th = Thread(target=pr.main_life_cycle, args=(pr, ))
+    th.start()
+
+    # відтворення життєвих циклів
+    # на основі activity diagram
+    with tpe(max_workers=3) as executor:
+        executor.submit(pr.breath)
+        executor.submit(pr.main_life_cycle)
+        executor.submit(pr.sleep)
+
+
+
+
+
+
+
+
